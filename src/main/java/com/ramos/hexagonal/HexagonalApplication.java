7bce92b4d3472594af7f790526072a0080cdf490
Jefferson Ramos <jefferson.ramos10@gmail.com>
2026-02-24 22:19:26 -0300

    Criando o Client

--- src/main/java/com/ramos/hexagonal/HexagonalApplication.java
+++ src/main/java/com/ramos/hexagonal/HexagonalApplication.java
@@ -2,7 +2,10 @@ package com.ramos.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// Foi add a notação "@EnableFeignClients" para habilitar o uso do MOCK de endereço
+@EnableFeignClients
 @SpringBootApplication
 public class HexagonalApplication {
 

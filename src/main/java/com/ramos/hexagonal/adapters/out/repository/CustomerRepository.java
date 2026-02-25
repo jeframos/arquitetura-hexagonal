74e54905923a8a14e855ef3ff7091e0f0ae9763a
Jefferson Ramos <jefferson.ramos10@gmail.com>
2026-02-24 22:50:05 -0300

    Criando o repository

---
+++ src/main/java/com/ramos/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.ramos.hexagonal.adapters.out.repository;
+
+import com.ramos.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}

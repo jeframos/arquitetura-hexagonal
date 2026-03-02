76d00e80097aea791b8fda6dcaf05af041466517
Jefferson Ramos <jefferson.ramos10@gmail.com>
2026-03-02 17:33:39 -0300

    Criando o UseCase de exclusão do cliente

---
+++ src/main/java/com/ramos/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,24 @@
+package com.ramos.hexagonal.application.core.usecase;
+
+import com.ramos.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.ramos.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(
+            FindCustomerByIdInputPort findCustomerByIdInputPort,
+            DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort
+    ) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+}
---
+++ src/main/java/com/ramos/hexagonal/application/ports/out/DeleteCustomerByIdOutputPort.java
@@ -0,0 +1,7 @@
+package com.ramos.hexagonal.application.ports.out;
+
+public interface DeleteCustomerByIdOutputPort {
+
+    void delete(String id);
+
+}
